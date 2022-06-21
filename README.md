8b46291a2d8f412b451006d9af9d89eb2cbe46e5
moss0801 <moss0801@gmail.com>
2022-06-22 00:55:27 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# file-extension-block
+# 파일 확장자 차단
+## 기술 스택
+* Front End : vue.js
+* Back End : spring boot
+* DB : h2
+
+## 고려 사항
+
---
+++ src/test/java/com/moss/fileextensionblock/FileExtensionBlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moss.fileextensionblock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileExtensionBlockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
